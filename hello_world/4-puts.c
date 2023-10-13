81be326993b70d9c66eef9debaeb92ac226c82c7
freddyrosa16 <freddy.rosa16@gmail.com>
2023-10-13 14:11:33 -0400

    Hello, puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/** 
+ * main - Principal function, and prints the function 
+ * Return: returns value 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
