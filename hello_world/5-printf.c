8e55871c14990fdd40e9379eaa09ff6dfd69d363
freddyrosa16 <freddy.rosa16@gmail.com>
2023-10-15 06:35:45 -0400

    Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Principal function, and prints the function
+ * Return: returns value 0
+ */
+int main ()
+{
+	printf ("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
