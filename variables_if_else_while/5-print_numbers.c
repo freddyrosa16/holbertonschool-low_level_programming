b92240d5843b11e07a86e9c5febffef73c77d900
freddyrosa16 <freddy.rosa16@gmail.com>
2023-10-16 14:06:33 -0400

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - first function, and prints the function
+ * Return: return value 0
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0' ; n <= '10' ; n++)
+	{
+		putchar (n)
+	}
+	putchar('\n')
+	return (0)
+}
