3c2598a8f1b8841bc5e9707d7e7aa2d00724636c
freddyrosa16 <freddy.rosa16@gmail.com>
2023-10-16 11:28:47 -0400

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Principal function, and prints the function
+ * Return: return value 0
+ */
+int main(void)
+{
+	char ch;
+	char chh
+
+	for (ch ='a', ch <= 'z', ch++)
+	{ putchar(ch); }
+	for (chh = 'A', chh <= 'Z', chh++)
+	{ putchar(chh); }
+	putchar('\n');
+	return (0);
+}
