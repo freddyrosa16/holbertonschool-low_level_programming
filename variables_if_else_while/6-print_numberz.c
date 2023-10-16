3b550755d663f9593e144a201484a8dd1c542447
freddyrosa16 <freddy.rosa16@gmail.com>
2023-10-16 14:20:05 -0400

    Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - first function, and prints the function
+ * Return: return value 0
+ */
+int main(void)
+{
+	int n;
+	for (n = 0 ; n < 10 ; n++)
+	{
+		putchar ('0' + n);
+	}
+	putchar('\n');
+	return (0);
+}
