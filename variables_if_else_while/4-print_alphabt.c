878c9eace8e4e4754b8a85af8642a1592339be69
freddyrosa16 <freddy.rosa16@gmail.com>
2023-10-16 13:58:17 -0400

    When I was having that alphabet soup, i never thought that it would pay off

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -5,13 +5,15 @@
  */
 int main(void)
 {
-	char ch;
+	char ch, e, q;
+	e = 'e';
+	q = 'q';
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{ putchar(ch); }
-	if (ch != 'q' && ch != 'e')
 	{
-	putchar('\n');
+	if (ch != q && ch != e)
+	putchar(ch);
 	}
+	putchar('\n');
 	return (0);
 }
