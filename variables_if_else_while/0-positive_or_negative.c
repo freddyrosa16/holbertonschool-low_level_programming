eca74a636962902beb3aea915fa7e60270d49798
freddyrosa16 <freddy.rosa16@gmail.com>
2023-10-16 10:07:49 -0400

    Positive anything is better than negative nothing

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- *  main - Principal function, prints the function depending on the number that is being generated. If a number is greater than 0 it will print os positive, else if the number is equal to 0 it will print is zero, else it will print is negative
+ *  main - Prints the function depending on the number that is being generated. If a number is greater than 0 it will print os positive, else if the number is equal to 0 it will print is zero, else it will print is negative
  *  Return: returns value 0
  */
 int main(void)
