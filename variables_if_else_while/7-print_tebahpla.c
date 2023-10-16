a58b0d7b16fd680afbef4488854dca17181d3fff
freddyrosa16 <freddy.rosa16@gmail.com>
2023-10-16 14:27:43 -0400

    Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - first function, prints the function
+ * Return: return value 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'z' ; l >= 'a' ; l++)
+	{putchar(l);}
+	return (0);
+}
