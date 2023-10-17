dc703bdb57a42a1c097cee38f4e7ea44bc13a68d
freddyrosa16 <freddy.rosa16@gmail.com>
2023-10-17 09:00:24 -0400

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - first function, prints the function
+ * Return: return value 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{putchar('0' + n); }
+	if (n < 10)
+	{putchar(', '); }
+	putchar('\n');
+	return (0);
+}
