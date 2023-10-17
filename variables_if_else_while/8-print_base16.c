afb826400e7d4ce3138862afdbc38585d9bd66ee
freddyrosa16 <freddy.rosa16@gmail.com>
2023-10-17 08:44:33 -0400

    Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - first function, prints the function
+ * Return: return value 0
+ */
+int main(void)
+{
+	int n;
+	char l;
+
+	for (n = 0 ; n < 10 ; n++)
+	{putchar('0' + n); }
+	for (l = 'a' ; l <= 'f' ; l++)
+	{putchar(l); }
+	putchar('\n')
+	return (0)
+}
