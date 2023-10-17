015ad75e8b7c55959d1d02a173aabcaa473d5145
freddyrosa16 <freddy.rosa16@gmail.com>
2023-10-17 15:04:21 -0400

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - Checks if its lowercase
+ */
+int _isalpha(int c)
+{
+	int i;
+	for (i = 97 ; i < 122 ; i++)
+	{
+		if (c == i)
+			return (1);
+	}
+	return (0);
+}
+
