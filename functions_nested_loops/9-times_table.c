188c2ac5545e97ac88fd00adfdf8f3dba825b4bb
freddyrosa16 <freddy.rosa16@gmail.com>
2023-10-18 17:32:19 -0400

    Learn your times tables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,23 +4,33 @@
  */
 void times_table(void)
 {
-	int i, j, k;
+	int i, j, k, l, m;
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
-		_putchar ('0');
 		for (j = 1 ; j <= 9 ; j++)
 		{
-			_putchar (',');
-			_putchar (' ');
 			k = i * j;
-			if (k <= 9);
-			
+			if (k > 9)
+			{
+				l = k % 10;
+				m = (k - l) / 10;
+				_putchar (44);
+				_putchar (32);
+				_putchar (m + '0');
+				_putchar (u + '0');
+			}
 			else
-			_putchar ((k / 10) + '0');
-			_putchar ((k % 10) + '0');
+			{
+				if (j != 0)
+				{
+					_putchar (44);
+					_putchar (32);
+					_putchar (32);
+				}
+				_puchar (k + '0');
+			}
 		}
 		_putchar ('\n');
 	}
 }
-
