3e844e64d8e1d4df34a401853e39156089e89727
freddyrosa16 <freddy.rosa16@gmail.com>
2023-10-19 08:56:49 -0400

    a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int add - is the addition of 2 intergers
+ * @a: interger 1
+ * @b: interger 2
+ * Return: return a + b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
+
+
+
