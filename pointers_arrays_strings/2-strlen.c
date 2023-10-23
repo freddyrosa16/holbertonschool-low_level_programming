84ff7053c624c2d8b404835d4572089275dd113b
freddyrosa16 <freddy.rosa16@gmail.com>
2023-10-23 13:23:30 -0400

    This report, by its very length, defends itself against the risk of being read

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * strlen - A function that meassures the length of a string.
+ * @s: string to evaluate.
+ * Return: return the value of the string.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
