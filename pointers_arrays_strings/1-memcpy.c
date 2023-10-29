405773f1c0222de3771a75f285f7fce8e4a39a6d
freddyrosa16 <freddy.rosa16@gmail.com>
2023-10-29 18:19:49 -0400

    memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,10 @@
 #include "main.h"
 /**
+ * _memcpy - copy memory area.
+ * @dest: to where Im sending the copy.
+ * @src: from where Im coping.
+ * @n: number of spaces to fill.
+ * Return: a pointer to dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
