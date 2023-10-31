218b27eeb7f7cb615bfdcfb32b2817c195849e20
freddyrosa16 <freddy.rosa16@gmail.com>
2023-10-31 09:41:36 -0400

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**_puts_recursion - Functio that prints a string.
+ * @s: String to be printed.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+	_putchar (*s);
+	s++;
+	}
+
+	if (*s == '\0')
+	{
+		_putchar ('\n');
+		return;
+	}
+
+	_puts_recursion (s);
+}
