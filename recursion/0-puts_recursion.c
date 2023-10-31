c6a2d224b3fffd3b371febb9325dc9fa7de0a050
freddyrosa16 <freddy.rosa16@gmail.com>
2023-10-31 09:42:56 -0400

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -6,15 +6,13 @@ void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-	_putchar (*s);
-	s++;
+		_putchar (*s);
+		s++;
 	}
-
 	if (*s == '\0')
 	{
 		_putchar ('\n');
 		return;
 	}
-
 	_puts_recursion (s);
 }
