5500ec51fdb1e77bb13876c3ccb39092a119f489
freddyrosa16 <freddy.rosa16@gmail.com>
2023-11-17 10:08:55 -0400

    Poopy

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,20 +1,6 @@
-#ifndef DOG_H
-#define DOG_H
-/**
- * struct dog - dog information
- * @name: dog name
- * @age: dog age
- * @owner: dog owner
- */
 typedef struct dog
 {
-char *name;
-float age;
-char *owner;
-} dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif
+  char *name;
+  float age;
+  char *owner;
+}
