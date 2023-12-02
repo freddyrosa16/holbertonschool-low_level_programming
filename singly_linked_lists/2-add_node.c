7f9afba816c02d9f2fff85fd81fdd49d386d39bc
freddyrosa16 <freddy.rosa16@gmail.com>
2023-12-02 13:48:33 -0400

    Add node at beginnig

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,9 @@
 #include "lists.h"
 /**
+ * add_node - functions that adds a new mode at the beginning of list
+ * @head: pointing to the beginning of the string
+ * @str: pointer to the string
+ * Return: the address of the new elements, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
